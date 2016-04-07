33c5c91cb62d27ef15056244b9760e0cae418e14
Gabriel Checchia Vitali <vitali.gabriel@gmail.com>
2016-04-07 17:37:54 -0300

    Version 1.3
    Overall organization on HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,32 @@
 angular.module('resumePage', ['ui.bootstrap']).controller('PageController', ['$scope',  function($scope, $console) { 
 
 	$scope.backgroundColor = "green";
+	$scope.selectedArea = 'assets/pages/profile.html';
 	$scope.skillPointClasses = "skill-level active animated infinite pulse";
 
     $scope.changePage = function(name) {
 	      if(name == "profile")
 	      {
 	      	$scope.backgroundColor = "green";
+	      	$scope.selectedArea = "assets/pages/profile.html";
 
 	      }
 	      else if(name == "knowledge")
 	      {
 	      	$scope.backgroundColor = "purple";
+	      	$scope.selectedArea = "assets/pages/knowledge.html";
 
 	      }
 	      else if(name == "resume")
 	      {
 	      	$scope.backgroundColor = "orange";
+	      	$scope.selectedArea = "assets/pages/resume.html";
 
 	      }
 	      else 
 	      {
 	      	$scope.backgroundColor = "pink";
+	      	$scope.selectedArea = "assets/pages/portfolio.html";
 
 	      }
     }
