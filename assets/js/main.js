50b71043a00a50dea5bfdc5710c48064034115d0
Gabriel Checchia Vitali <vitali.gabriel@gmail.com>
2016-04-13 15:01:56 -0300

    1.4.0
    Now the website triggers events for Google Analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,30 +7,32 @@ angular.module('resumePage', ['ui.bootstrap']).controller('PageController', ['$s
     $scope.changePage = function(name) {
 	      if(name == "profile")
 	      {
+	      	ga('send', 'event', 'page', 'open', 'Profile Page');
 	      	$scope.backgroundColor = "green";
 	      	$scope.selectedArea = "assets/pages/profile.html";
 
 	      }
 	      else if(name == "knowledge")
 	      {
+	      	ga('send', 'event', 'page', 'open', 'Knowledge Page');
 	      	$scope.backgroundColor = "purple";
 	      	$scope.selectedArea = "assets/pages/knowledge.html";
 
 	      }
 	      else if(name == "resume")
 	      {
+	      	ga('send', 'event', 'page', 'open', 'Resume Page');
 	      	$scope.backgroundColor = "orange";
 	      	$scope.selectedArea = "assets/pages/resume.html";
 
 	      }
 	      else 
 	      {
+	      	ga('send', 'event', 'page', 'open', 'Portfolio Page');
 	      	$scope.backgroundColor = "pink";
 	      	$scope.selectedArea = "assets/pages/portfolio.html";
 
 	      }
     }
-
-
 
 }]);
