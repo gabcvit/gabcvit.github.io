12a21b1c4769e45e86f365c5b5a9aca7f793343f
Gabriel Vitali <vitali.gabriel@gmail.com>
2022-09-04 16:29:28 +0200

    Revert "Revert "First setup of v2""
    This reverts commit e8ffea3ac24e608ec5db6b408474b06ee91e76cd.

---
+++ v2/gabcvit.github.io/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="grid grid-cols-3 h-screen w-screen place-items-center bg-stone-900">
+    <div class="col-span-1 w-full h-screen">
+      <NavigationBar @onNewPageSelected="renderComponentForOptionId" />
+    </div>
+    <div class="col-span-2 w-full h-screen overflow-y-scroll">
+      <component :is="currentTab" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import ProfileComponent from './components/ProfileComponent.vue'
+  import KnowledgeComponent from './components/KnowledgeComponent.vue'
+  import ResumeComponent from './components/ResumeComponent.vue'
+  import PortfolioComponent from './components/PortfolioComponent.vue'
+  import NavigationBar from './components/NavigationBar.vue'
+  import {reactive, ref} from "vue";
+
+  export default {
+    components: {
+      ProfileComponent,
+      KnowledgeComponent,
+      ResumeComponent,
+      PortfolioComponent,
+      NavigationBar,
+    },
+    data() {
+      return {
+        currentTab: 'ProfileComponent',
+        tabs: {
+          profile: {
+            component: 'ProfileComponent'
+          },
+          knowledge: {
+            component: 'KnowledgeComponent'
+          },
+          resume: {
+            component: 'ResumeComponent'
+          },
+          portfolio: {
+            component: 'PortfolioComponent'
+          }
+        }
+      }
+    },
+    methods: {
+      renderComponentForOptionId(optionId: string) {
+        if (this.tabs[optionId] !== null) {
+          this.currentTab = this.tabs[optionId].component
+        }
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ v2/gabcvit.github.io/src/components/NavigationBarClickableOption.vue
@@ -0,0 +1,27 @@
+<template>
+  <p class="hover:text-indigo-600 text-2xl cursor-pointer px-24"
+     @click="onOptionClicked">{{ getNameForOptionId() }}</p>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+
+const emit = defineEmits<{
+  (e: 'onOptionClicked', optionId: string): void
+}>()
+
+const props = defineProps(['optionId'])
+
+const onOptionClicked = (optionId: string) => {
+  emit('onOptionClicked', props.optionId)
+};
+
+const getNameForOptionId = () => {
+  return props.optionId.charAt(0).toUpperCase()
+      + props.optionId.slice(1)
+};
+</script>
+
+<style scoped>
+
+</style>
