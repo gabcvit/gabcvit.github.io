d969b5739f88dfa9762bdd3434b9f94b0eda3ad1
Gabriel Vitali <vitali.gabriel@gmail.com>
2022-09-04 16:28:16 +0200

    First setup of v2
    - Initialized new VueJS + Tailwind app (all in temporary v2 folder)
    - Pasted all content from the previous website in separate components
    - Implemented navigation bar and actions
    - Updated content from ProfileComponent and Knowledge Component to match new scope

---
+++ v2/gabcvit.github.io/src/components/KnowledgeComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <p class="text-2xl my-16">Stats</p>
+
+  <p class="text-xl my-12 underline-offset-4 underline">Technical skills</p>
+
+  <p>UX / UI Design</p>
+  <p class="mb-12">4 / 5</p>
+
+  <p>Frontend Development</p>
+  <p class="mb-12">5 / 5</p>
+
+  <p>Backend Development</p>
+  <p class="mb-12">3 / 5</p>
+
+  <p>Test-Driven Development (TDD)</p>
+  <p class="mb-12">3 / 5</p>
+
+  <p>Test-Driven Development (TDD)</p>
+  <p class="mb-12">4 / 5</p>
+
+  <p>Infra</p>
+  <p class="mb-12">2 / 5</p>
+
+  <p class="text-xl my-12 underline-offset-4 underline">Consulting skills and Business Analysis</p>
+
+  <p>Communication skills</p>
+  <p class="mb-12">5 / 5</p>
+
+  <p>Agile methodologies</p>
+  <p class="mb-12">4 / 5</p>
+
+  <p>Navigating ambiguity</p>
+  <p class="mb-12">4 / 5</p>
+
+  <p>Business Analysis</p>
+  <p class="mb-12">3.5 / 5</p>
+
+  <p class="text-xl my-12 underline-offset-4 underline">Things I can work with</p>
+  <p class="my-4 text-lg">Frontend</p>
+  <ul>
+    <li>Swift</li>
+    <li>VueJS</li>
+    <li>ReactJS / React-native</li>
+    <li>Unity 3D C#</li>
+    <li>GraphQL</li>
+    <li>Wordpress</li>
+    <li>Android (Java)</li>
+    <li>Ionic Framework v1, v2, v3</li>
+    <li>AngularJS</li>
+    <li>HTML5</li>
+    <li>CSS3</li>
+    <li>GitHub Actions</li>
+    <li>NodeJS</li>
+  </ul>
+  <p class="my-4 text-lg">Design tools</p>
+  <ul>
+    <li>Sketchapp</li>
+    <li>Figma</li>
+    <li>InVision</li>
+    <li>Affinity designer</li>
+    <li>Affinity Photo</li>
+    <li class="mb-24">Adobe Photoshop</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "KnowledgeComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
