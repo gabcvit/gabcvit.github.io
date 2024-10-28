4aaec5465a4a9495e3415f9acf843bdb41e9f08a
Gabriel Vitali <vitali.gabriel@gmail.com>
2024-10-28 23:18:22 +0100

    #14 Making screen more compatible for mobile screens
    - Added media query support for not splitting the screen in half vertically always, when it's open in a smaller screen, it should allow a better usage of the portrait screen format
    - NavigationBarRouterLink now supports "preselected" state, it will indicate what page is currently being shown for the user

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="grid grid-cols-4 h-screen w-screen place-items-center bg-stone-900">
-    <div class="col-span-1 w-full h-screen pl-16">
+  <div class="grid md:grid-cols-4 h-screen w-screen place-items-center bg-stone-900">
+    <div class="md:col-span-1 w-full h-screen pl-4 md:pl-16">
       <NavigationBar />
     </div>
-    <div class="col-span-3 w-full h-screen overflow-y-scroll pr-16">
+    <div class="md:col-span-3 w-full h-screen md:overflow-y-scroll px-4 md:pl-0 md:pr-16">
       <router-view />
     </div>
   </div>
