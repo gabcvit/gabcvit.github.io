07f053ef53795266462d2ef567e1b608464c4a65
Gabriel Vitali <vitali.gabriel@gmail.com>
2025-02-03 01:12:39 +0100

    #30 Adding RSS link and improving `SocialLinksWrapper`
    - The component supports now flex alignment and dynamic wrapping of components, better mobile support (by not covering menu options in smaller devices) and better width management

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid md:grid-cols-4 h-screen w-screen place-items-center">
-    <div class="md:col-span-1 w-full h-screen pl-4 md:pl-16">
+    <div class="md:col-span-1 w-full h-screen pl-4 md:pl-16 relative">
       <NavigationBar />
     </div>
     <div class="md:col-span-3 w-full h-screen md:overflow-y-scroll px-4 md:pl-0 md:pr-16 pb-16">
--- src/components/SocialLinksWrapper.vue
+++ src/components/SocialLinksWrapper.vue
@@ -1,28 +1,36 @@
 <template>
-  <div class="absolute bottom-0 left-0 p-5 w-52 bg-gray-900 rounded-tr-2xl">
-    <p class="m-1">
-      Social links:
+  <div class="absolute bottom-0 left-0 p-5 w-[90%] bg-gray-900 rounded-tr-2xl">
+    <p class="mb-4">
+      Let's connect! 🚀
     </p>
-    <TagSocial
-      iconUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
-      label="GitHub"
-      url="https://github.com/gabcvit"
-    />
-    <TagSocial
-      iconUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/stackoverflow/stackoverflow-original.svg"
-      label="Stackoverflow"
-      url="https://stackoverflow.com/users/6231562/gabcvit"
-    />
-    <TagSocial
-      iconUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg"
-      label="LinkedIn"
-      url="https://www.linkedin.com/in/gabcvit/"
-    />
-    <TagSocial
-      iconUrl="https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg"
-      label="BlueSky"
-      url="https://bsky.app/profile/gabcvit.bsky.social"
-    />
+    <div class="flex flex-start flex-wrap gap-2">
+      <TagSocial
+        iconUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+        label="GitHub"
+        url="https://github.com/gabcvit"
+      />
+      <TagSocial
+        iconUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/stackoverflow/stackoverflow-original.svg"
+        label="Stackoverflow"
+        url="https://stackoverflow.com/users/6231562/gabcvit"
+      />
+      <TagSocial
+        iconUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg"
+        label="LinkedIn"
+        url="https://www.linkedin.com/in/gabcvit/"
+      />
+      <TagSocial
+        iconUrl="https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg"
+        label="BlueSky"
+        url="https://bsky.app/profile/gabcvit.bsky.social"
+      />
+      <TagSocial
+        iconUrl="https://cdn2.iconfinder.com/data/icons/social-icon-3/512/social_style_3_rss-1024.png"
+        label="RSS"
+        url="https://gabcvit.github.io/feed.xml"
+      />
+    </div>
+    
   </div>
 </template>
 
