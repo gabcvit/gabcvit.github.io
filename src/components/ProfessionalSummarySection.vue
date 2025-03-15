e2aeef5ee6b1e9c90d25bef8473af3cf336b9f6a
Gabriel Vitali <vitali.gabriel@gmail.com>
2025-03-15 19:17:59 +0100

    Improving professional summary
    - Added "Core Skills" section as well for more transparency on non-dev skills

--- src/components/ProfessionalSummarySection.vue
+++ src/components/ProfessionalSummarySection.vue
@@ -6,20 +6,31 @@ import SectionHeader from "../components/SectionHeader.vue";
   <SectionHeader title="Professional summary" />
   <div class="section-wrapper blog-post">
     <p class="text-sm mb-2">
-      Experienced software engineer and consultant with 10+ years in building scalable web and mobile
-      solutions across diverse industries. I specialize in bridging technical and business needs,
-      ensuring clarity, efficiency, strategic alignment and high quality software delivery.
+      Experienced and Leadership-thinking Web and Mobile applications engineer and consultant with over
+      10 years in building scalable Web and Mobile solutions for multiple industries. I specialize in
+      aligning complex technical knowledge with business requirements, ensuring clarity, high impact,
+      team efficiency and healthy collaboration, transparent alignment and finally high quality software delivery. 
     </p>
     <p class="text-sm mb-2">
       Track record:
     </p>
     <ul>
+      <li>Heavy technical knowledge in web and mobile applications languages, frameworks, cloud solutions and Agile methodologies </li>
       <li>10+ successful app launches, impacting thousands of users.</li>
       <li>20+ client collaborations, delivering tailored tech solutions.</li>
       <li>Multiple industry certifications in key web and mobile technologies.</li>
     </ul>
-    <p class="text-sm">
-      Currently working in Germany 🇩🇪
-    </p>
+  </div>
+  <SectionHeader title="Core Skills" />
+  <div class="section-wrapper blog-post">
+    <ul>
+      <li>Business Administration </li>
+      <li>Customer Experience and Accessibility </li>
+      <li>CI/CD and Pipeline optimization</li>
+      <li>Stakeholder management and influencing </li>
+      <li>Team leadership for bigger impact </li>
+      <li>Agile methodologies</li>
+      <li>Information security and data protection</li>
+    </ul>
   </div>
 </template>
