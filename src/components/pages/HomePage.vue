97c9d3c7171f05535fd3b66573922cfe95100540
Gabriel Vitali <vitali.gabriel@gmail.com>
2025-03-16 17:41:02 +0100

    Updated HomePage with a more actual content

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -6,14 +6,14 @@
       Nice to have you here! I'm Gabriel 🚀
     </p>
     <p>
-      I’m a software developer and consultant who thrives on solving complex technical challenges, driving strategic initiatives, and fostering collaboration across teams. With experience in mentoring developers, guiding technical decisions, and optimizing workflows, I build scalable web and mobile solutions that push the industry forward.
+      I’m an experienced and Leadership-focused Web and Mobile applications engineer with 10+ years in delivering scalable Web and Mobile solutions, leading cross-functional teams and aligning technical decisions with business goals across multiple industries. I focus on empowering teams by fostering a culture of continuous improvement, clear communication, and technical excellence. Passionate about challenging code bias, GenAI, decentralization and meditation practices.
     </p>
     <p>
-      In this website you'll find more about my professional journey, personal projects, and thoughts on the tech industry:
+      In this website you'll find more about my professional journey, personal projects, and self-reflections about the latest trends in the Tech industry:
     </p>
     <ul>
-      <li><a href="./resume">Resume</a>: My summarized technical CV and experience in the industry</li>
-      <li><a href="./blog">Blog</a>: Thought-provoking posts on software, ways of working, emerging and equitable tech, reflections on GenAI and code biases.</li>
+      <li><a href="./resume">Resume</a>: My full CV with career achievements and skills</li>
+      <li><a href="./blog">Blog</a>: Thought-provoking posts on Software, Ways of Working, emerging and equitable Tech, reflections on GenAI and code biases.</li>
       <li><a href="./portfolio">Portfolio</a>: My latest personal projects and case studies</li>
     </ul>
   </div>
@@ -21,7 +21,7 @@
   <SectionHeader title="Let’s connect!" />
   <div class="blog-post orange-links">
     <p>
-      Whether you’re a fellow developer, an industry leader, or just curious about tech, I hope you find something here that sparks your interest.
+      Whether you’re a fellow developer, an industry leader, or just curious about the topics I write about, I hope you find something here that sparks your interest!
     </p>
     <p>
       Feel free to reach out via my social links or drop me a message, always happy to chat about exciting ideas, collaborations and to receive feedback.
