4ecd179d997de3354ff874aa06255af9ef2968ad
Gabriel Vitali <vitali.gabriel@gmail.com>
2025-03-29 18:37:52 +0100

    Refactoring `NavigationBar` component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,32 +1,24 @@
 <template>
   <nav class="h-screen w-1/2 grid content-center space-y-10">
     <NavigationBarRouterLink
-      label="Home"
-      optionId="/"
-      color="orange"
-    />
-    <NavigationBarRouterLink
-      label="Resume"
-      optionId="/resume"
-      color="indigo"
-    />
-    <NavigationBarRouterLink
-      label="Blog"
-      optionId="/blog"
-      color="pink"
-    />
-    <NavigationBarRouterLink
-      label="Portfolio"
-      optionId="/portfolio"
-      color="emerald"
+      v-for="(link, index) in navigationLinks"
+      :key="index"
+      :label="link.label"
+      :optionId="link.optionId"
+      :color="link.color"
     />
   </nav>
   <SocialLinksWrapper />
 </template>
 
 <script setup lang="ts">
-
 import NavigationBarRouterLink from "./NavigationBarRouterLink.vue";
-import SocialLinksWrapper from './SocialLinksWrapper.vue'
+import SocialLinksWrapper from './SocialLinksWrapper.vue';
 
+const navigationLinks = [
+  { label: "Home", optionId: "/", color: "orange" },
+  { label: "Resume", optionId: "/resume", color: "indigo" },
+  { label: "Blog", optionId: "/blog", color: "pink" },
+  { label: "Portfolio", optionId: "/portfolio", color: "emerald" },
+];
 </script>
