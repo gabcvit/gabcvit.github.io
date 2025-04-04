6c9fbac04fec751d34832da11e18d113a16d16bd
Gabriel Vitali <vitali.gabriel@gmail.com>
2025-04-05 00:13:41 +0200

    #56 Expanding test from ResumePage

--- cypress/e2e/ResumePage.cy.ts
+++ cypress/e2e/ResumePage.cy.ts
@@ -1,12 +1,22 @@
+import { languages } from "../../src/data/languagesData";
+
 describe("ResumePage", () => {
-	it("should load the resume page", () => {
-	  cy.visit("/resume");
-	  cy.contains("Professional summary").should("be.visible");
-	  cy.contains("Core Skills").should("be.visible");
-	  cy.contains("Tech Skills").should("exist");
-	  cy.contains("Professional Experience").should("exist");
-	  cy.contains("Education and Certifications").should("exist");
-	  cy.contains("Language skills").should("exist");
+	beforeEach(() => {
+		cy.visit("/resume");
 	});
-  });
-  
+
+	it("should load the resume page", () => {
+		cy.contains("Professional summary").should("be.visible");
+		cy.contains("Core Skills").should("be.visible");
+		cy.contains("Tech Skills").should("exist");
+		cy.contains("Professional Experience").should("exist");
+		cy.contains("Education and Certifications").should("exist");
+	});
+
+	it("should display the LanguagesSection correctly", () => {
+		cy.contains("Language skills").should("exist");
+		languages.forEach((language) => {
+			cy.contains(`${language.name}: ${language.level}`).should("exist");			
+		});
+	});
+});
