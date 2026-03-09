f24d83cb200e085aeba81c590d4fe2d622fe1ff5
Gabriel Vitali <vitali.gabriel@gmail.com>
2026-03-09 20:57:35 +0100

    Updating pipeline test

--- cypress/e2e/ResumePage.cy.ts
+++ cypress/e2e/ResumePage.cy.ts
@@ -7,7 +7,6 @@ describe("ResumePage", () => {
 
 	it("should load the resume page", () => {
 		cy.contains("Professional summary").should("be.visible");
-		cy.contains("Core Skills").should("be.visible");
 		cy.contains("Tech Skills").should("exist");
 		cy.contains("Professional Experience").should("exist");
 		cy.contains("Education and Certifications").should("exist");
